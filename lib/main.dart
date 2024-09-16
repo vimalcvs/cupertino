8e1ea78f43fb3230f779009141b670865f5c9e5b
TECHNOVIMAL <vimalcvs29@gmail.com>
2024-09-16 23:59:53 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'app.dart';
+import 'model/app_state_model.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: const CupertinoStoreApp(),
+    ),
+  );
+}
